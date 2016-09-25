d4f3325e0ddbb8226006d2488e47a4037f61fcdb
CFP <10453041@email.ntou.edu.tw>
2016-09-25 22:14:30 +0800

    新增更新辛酸史，更新至2016/09/26

---
+++ resources/views/client/log.blade.php
@@ -0,0 +1,33 @@
+@extends('layout.default')
+@section('wrapper')
+<section class="main clearfix">
+  <section class="top"> 
+    <div class="wrapper content_header clearfix">
+      <div class="title">更新辛酸史</div>
+      <div class="btn-group">
+    <div class="form-inline">
+  </div>
+  </div>
+</div>
+    </section><!-- end top -->
+
+    <section class="wrapper">
+
+      <div class="content">
+      <dl>
+        <dt><h3>2016/09/26 錯誤修正</h3></dt>
+        <dd>地圖店家點擊錯誤修正</dd>
+        <dd>匯入菜單格式檢查改善</dd>
+        <dd>回傳訊息字體改善</dd>
+        <dd>新增店家頁面多了警告訊息</dd>
+        <dt><h3>2016/09/24 GitHub導入</h3></dt>
+        <dt><h3>2016/09/23 系統正式上線</h3></dt>
+        <dd>感謝博班彭學長IP資訊協助</dd>
+        <dt><h3>2016/08/23 確認網頁版型後正式開工</h3></dt>
+        <dd>一條不歸路的開始</dd>
+      </dl>
+
+      </div><!-- end content -->
+    </section>
+  </section><!-- end main -->
+  @stop
