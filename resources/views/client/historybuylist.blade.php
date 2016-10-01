b7f385a1e8c5c2578e599030eba12ea7ea3df9f4
cat811212 <peng-macbook@Peng-Macbookde-MacBook-Pro.local>
2016-10-01 21:18:45 +0800

    金額結算介面修改，新增其他人訂了什麼連結

--- resources/views/client/historybuylist.blade.php
+++ resources/views/client/historybuylist.blade.php
@@ -5,8 +5,11 @@
 		<section class="top">	
 			<div class="wrapper content_header clearfix">
   <div class="title">{{$shopName}}</div>
-  <div class="btn-group">
-  <div class="form-inline">
+<div class="btn-group">
+  	<div class="form-inline">
+		<a class="btn btn-primary" href="{{URL::asset('order/'.$group_id.'?view=people')}}">其他人訂了什麼？</a>
+	</div>
+  </div>
   <?php $total=0;$items=0;
   foreach($order as $list){
   	$items=1+$items;
@@ -15,24 +18,23 @@
 
 
   ?>
-      總共有 <div id="selected" style="display:inline-block;">{{$items}}</div> 項 總計：<div id="total" style="display:inline-block;">{{$total}}</div>元
-      </div>
-	</div>
+  
 </div>
 		</section><!-- end top -->
 		<section class="wrapper">
 
 			<div class="content">
+			
 			<div class="row">
   <div class="col-md-6">
   	<h4>電話：{{$shopInfo[0]['tel']}}</h4>
   	<h4>備註：{{$shopInfo[0]['memo']}}</h4>
   </div>
 </div>
+
 				<table class="table table-hover">
 					<thead>
 						<tr>
-							<th class="col-md-2">名稱</th>
 							<th class="col-md-4">訂購內容</th>
 							<th class="col-md-1">數量</th>
 							<th class="col-md-1">總額</th>
@@ -44,7 +46,6 @@
 					@foreach($order as $list)
 
 						<tr>
-							<th>{{$list['member']}}</th>
 							<th>
 								@if($list['parent']==null)
 
@@ -62,6 +63,9 @@
 
 					</tbody>
 				</table>
+				<div class="order-total" style="text-align: right;margin: 15px 0; color: #FFF;background-color: #328bca;padding: 10px;">
+  	總共有&nbsp<div id="selected" style="font-size: 24px;display:inline-block;">{{$items}}</div>&nbsp項&nbsp&nbsp&nbsp總計：&nbsp<div id="total" style="font-size: 24px;display:inline-block;">{{$total}}</div>&nbsp元
+  </div>
 			</div><!-- end content -->
 		</section>
 		</section><!-- end main -->
