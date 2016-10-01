f6a35bd9e6fc4174028c456bd068f24713608121
cat811212 <peng-macbook@Peng-Macbookde-MacBook-Pro.local>
2016-10-01 21:16:20 +0800

    新增檢查使用者是否存在

--- app/Service/MemberService.php
+++ app/Service/MemberService.php
@@ -19,6 +19,18 @@ class MemberService{
     {
         return $this->memberRepository->getAllMember();
     }
+    /**
+     * 檢查使用者是否存在
+     * @param (int)使用者編號
+     * @return (bool)存在回傳true, 不存在回傳false
+     */
+    public function checkMember($user_id)
+    {
+        if($this->memberRepository->checkMember($user_id)){
+            return true;
+        }
+        return false;
+    }
 }
 
 ?>
