0b07ab18cc66ea1081579724b081c3c851717f35
cat811212 <peng-macbook@Peng-Macbookde-MacBook-Pro.local>
2016-10-10 21:25:35 +0800

    新增操作確認視窗

--- public/js/action.js
+++ public/js/action.js
@@ -2,29 +2,42 @@ $(document).ready(function() {
 	$("a[action][data]").click(function() {
         var action=$(this).attr('action');
         var data=$(this).attr('data');
-        p(action,data);
+        return p(action,data);
 
     });
     function p(url,data) {
-    	$form = $('<form action="'+url+'" method="POST"></form>');
-    	$form.append('<input type="hidden" name="_token" value="'+$('meta[name="csrf-token"]').attr('content')+'">');
-    	switch(url){
+        var m=null;
+        var e=null;
+      	switch(url){
     		case 'removeorder':
-    			$form.append('<input type="hidden" name="order_id" value="'+data+'">');
+                m="你確定要移除這個餐點？？";
+                e='<input type="hidden" name="order_id" value="'+data+'">';
     		break;
     		case 'removegroup':
-    			$form.append('<input type="hidden" name="group_id" value="'+data+'">');
+                m="你確定要刪掉這團？？";
+                e='<input type="hidden" name="group_id" value="'+data+'">';
     		break;
     		case 'stopgroup':
-    		    $form.append('<input type="hidden" name="group_id" value="'+data+'">');
+                m="你確定所有人都訂完了？？";
+                e='<input type="hidden" name="group_id" value="'+data+'">';
      		break;
      		case 'delshop':
-     			$form.append('<input type="hidden" name="shop_id" value="'+data+'">');
-     			break;
+                m="要刪除這家店？？";
+                e='<input type="hidden" name="shop_id" value="'+data+'">';
+     		break;
 
-    	}
-    	$('body').append($form);
-
-        $('form[action="'+url+'"]').submit();
+    	}	
+        if(m==null||e==null)
+            return false;
+        var r = confirm(m);
+        if(r == true){
+            $f = $('<form action="'+url+'" method="POST"></form>');
+            $f.append('<input type="hidden" name="_token" value="'+$('meta[name="csrf-token"]').attr('content')+'">');
+            $f.append(e);
+            $('body').append($f);
+            $('form[action="'+url+'"]').submit();
+            return true;
+        }
+        return false;
     }	
 });
