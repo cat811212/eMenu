a02cb21b32728eb2786503ca9df9b8720e37e3c1
cat811212 <peng-macbook@Peng-Macbookde-MacBook-Pro.local>
2016-10-10 21:24:39 +0800

    新增軟刪除

--- app/ShopMenu.php
+++ app/ShopMenu.php
@@ -3,11 +3,10 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class ShopMenu extends Model
 {
+	use SoftDeletes;
     protected $table = 'shop_menu';
     protected $fillable =['shop_id','name','price','parent'];
-    public $timestamps  = false;
-
 }
